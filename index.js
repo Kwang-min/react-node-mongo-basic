f470f7df123fab838b79d94a9c8fd32eec8bbfe5
min <j2h2677@gmail.com>
2021-02-23 21:06:05 +0900

    secret info

--- index.js
+++ index.js
@@ -2,8 +2,15 @@ const express = require('express')
 const app = express()
 const port = 5000
 const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
+const { User } = require('./models/User')
 
-mongoose.connect('mongodb+srv://min:abcd1234@react-node-mongo-basic.9mlea.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',{
+const config = require('./config/key')
+
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+
+mongoose.connect(config.mongoURI,{
   useNewUrlParser:true,useUnifiedTopology:true,useCreateIndex:true,useFindAndModify:false
 }).then(() => {console.log('mongoDB connected')})
 .catch(err => console.log(err))
@@ -11,5 +18,15 @@ mongoose.connect('mongodb+srv://min:abcd1234@react-node-mongo-basic.9mlea.mongod
 
 
 app.get('/', (req,res) => res.send("Hello word"))
+
+app.post('/register', (req,res) => {
+  const user = new User(req.body)
+  user.save((err,doc) => {
+    if(err) return res.json({ success:false, err})
+    return res.status(200).json({
+      success:true
+    })
+  })
+})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}`))
